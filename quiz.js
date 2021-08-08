948ec96ea826d2c2da118051c3ff67841f624bfa
chenben514 <86956296+chenben514@users.noreply.github.com>
2021-08-08 19:08:48 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+
 var quiz = {
   // (A) PROPERTIES
   // (A1) QUESTIONS & ANSWERS
@@ -55,7 +56,7 @@ var quiz = {
       quiz.hAns = document.createElement("div");
       quiz.hAns.id = "quizAns";
       quiz.hWrap.appendChild(quiz.hAns);
-    }
+    };
 
     //ben_test
     var selCategory = document.getElementById("category").value;
@@ -74,21 +75,25 @@ var quiz = {
       var r = Math.floor(Math.random() * quesCnt);
       if(quiz.quesList.indexOf(r) === -1) quiz.quesList.push(r);
     }
-    quiz.draw();
+
+    var selKind=document.getElementById("kind").value;
+     if (selKind==="select"){
+       quiz.draw_select();
+     } else if (selKind==="direct"){
+       quiz.draw_direct();
+     } else {
+       alert("nothing");
+     };
   },
 
-  // (C) DRAW QUESTION
-  draw: function(){
-     //alert(quesArr[quesList[quiz.now]]);
-    // (C1) QUESTION
+  // (C_1_SELECT) DRAW_SELECT QUESTION
+  draw_select: function(){
+    // (1) QUESTION
     var singQuesArr = quiz.quesArr[quiz.quesList[quiz.now]].split(',');
     quiz.hQn.innerHTML = "["+ (quiz.score+1) + "/" +   quiz.quesList.length.toString() + "] " + singQuesArr[0];
     quiz.correctAnswer = singQuesArr[1];
 
-    //quiz.hQn.innerHTML = questArr[quiz.quesList[quiz.now]];
-    //quiz.hQn.innerHTML = quesArr[quesList[quiz.now]];
-    // (C2) OPTIONS
-
+    // (2) OPTIONS
     quiz.ansList=[];
     quiz.correctPos=0;
     while(quiz.ansList.length < 4){
@@ -104,31 +109,25 @@ var quiz = {
     quiz.hAns.innerHTML = "";
     var i;
     for (i=0; i<=3; i++) {
-      // let radio = document.createElement("input");
-      // radio.type = "radio";
-      // radio.name = "quiz";
-      // radio.id = "quizo" + i;
-      // quiz.hAns.appendChild(radio);
       let label = document.createElement("label");
       label.innerHTML = singQuesArr[quiz.ansList[i]];
       quiz.ansString[i]=singQuesArr[quiz.ansList[i]];
       label.setAttribute("for", "quizo" + i);
       label.dataset.idx = i;
-      label.addEventListener("click", quiz.select);
+      label.addEventListener("click", quiz.draw_select_answer);
       quiz.hAns.appendChild(label);
-      // document.appendChild("<p>");
     }
   },
 
-  // (D) OPTION SELECTED
-  select: function(){
-    // (D1) DETACH ALL ONCLICK
+  // (C_1_ANSWER) OPTION SELECTED
+  draw_select_answer: function(){
+    // (1) DETACH ALL ONCLICK
     let all = quiz.hAns.getElementsByTagName("label");
     for (let label of all) {
       label.removeEventListener("click", quiz.select);
     }
 
-    // (D2) CHECK IF CORRECT
+    // (2) CHECK IF CORRECT
 //    alert("you press " + this.dataset.idx + " and correct is " + quiz.correctPos);
     let correct = this.dataset.idx == quiz.correctPos;
     if (correct) {
@@ -140,7 +139,7 @@ var quiz = {
         quiz.restart();
       }
       setTimeout(function(){
-        if (quiz.now < quiz.quesList.length) { quiz.draw(); }
+        if (quiz.now < quiz.quesList.length) { quiz.draw_select(); }
         else {
           quiz.hQn.innerHTML = `你 ${quiz.data.length} 題全部答對了！！`;
           quiz.hAns.innerHTML = "";
@@ -152,6 +151,94 @@ var quiz = {
       //  alert("你答錯了。\r\n[問題]："+quiz.hQn.innerHTML+"\r\n[正確答案]: "+quiz.correctAnswer+"\r\n[你回答了]: "+choiceAns);
       quiz.restart();
     }
-  }
-};
+  },
+
+
+
+  // (C_2_DIRECT) DRAW_DIRECT QUESTION
+  draw_direct: function(){
+    // (1) QUESTION
+    var singQuesArr = quiz.quesArr[quiz.quesList[quiz.now]].split(',');
+    quiz.hQn.innerHTML = "["+ (quiz.score+1) + "/" +   quiz.quesList.length.toString() + "] " + singQuesArr[0];
+    quiz.correctAnswer = singQuesArr[1];
+
+    // (2) OPTIONS
+    quiz.hAns.innerHTML = "";
+    var input = document.createElement("input");
+    input.type = "text";
+    input.className = "quizDirectAns"; // set the CSS class
+    quiz.hAns.appendChild(input);
+    document.querySelector(".quizDirectAns").focus();
+  },
+
+  //
+  // // (C_2_ANSWER) OPTION ANSWERED
+  // draw_select: function(){
+  //   // (1) DETACH ALL ONCLICK
+  //   let all = quiz.hAns.getElementsByTagName("label");
+  //   for (let label of all) {
+  //     label.removeEventListener("click", quiz.select);
+  //   };
+
+    // (2) CHECK IF CORRECT
+//    alert("you press " + this.dataset.idx + " and correct is " + quiz.correctPos);
+//     let correct = this.dataset.idx == quiz.correctPos;
+//     if (correct) {
+//       quiz.score++;
+//       // (D3) NEXT QUESTION OR END GAME
+//       quiz.now++;
+//       if (quiz.score===quiz.quesList.length){
+//         alert("你全部答對了!!!");
+//         quiz.restart();
+//       }
+//       setTimeout(function(){
+//         if (quiz.now < quiz.quesList.length) { quiz.draw(); }
+//         else {
+//           quiz.hQn.innerHTML = `你 ${quiz.data.length} 題全部答對了！！`;
+//           quiz.hAns.innerHTML = "";
+//         }
+//       }, 100);
+//     } else {
+//       var choiceAns = quiz.ansString[this.dataset.idx];
+//        alert("你答錯了。\r\n"+this.dataset.idx+quiz.correctPos+ "[問題]："+quiz.hQn.innerHTML+"\r\n[正確答案]: "+quiz.correctAnswer+"\r\n[你回答了]: "+choiceAns);
+//       //  alert("你答錯了。\r\n[問題]："+quiz.hQn.innerHTML+"\r\n[正確答案]: "+quiz.correctAnswer+"\r\n[你回答了]: "+choiceAns);
+//       quiz.restart();
+//     }
+//   }
+
+ };
+
+ document.addEventListener("keypress", function(event) {
+   if (event.key === "Enter") {
+     //alert(document.querySelector(".quizDirectAns").value);
+     //alert(quiz.correctAnswer);
+     let yourAnswer = document.querySelector(".quizDirectAns").value;
+     let correct = yourAnswer == quiz.correctAnswer;
+     if (correct) {
+          quiz.score++;
+           // (D3) NEXT QUESTION OR END GAME
+          quiz.now++;
+          if (quiz.score===quiz.quesList.length){
+             alert("你全部答對了!!!");
+             quiz.restart();
+           }
+          setTimeout(function(){
+             if (quiz.now < quiz.quesList.length) { quiz.draw_direct(); }
+             else {
+               quiz.hQn.innerHTML = `你 ${quiz.data.length} 題全部答對了！！`;
+               quiz.hAns.innerHTML = "";
+             }
+           }, 100);
+         } else {
+            alert("你答錯了。\r\n"+ "[問題]："+quiz.hQn.innerHTML+"\r\n[正確答案]: "+quiz.correctAnswer+"\r\n[你回答了]: "+yourAnswer);
+           //  alert("你答錯了。\r\n[問題]："+quiz.hQn.innerHTML+"\r\n[正確答案]: "+quiz.correctAnswer+"\r\n[你回答了]: "+choiceAns);
+           quiz.restart();
+         }
+   }else{
+     console.log("hello");
+     //consoalert(event.key);
+   }
+
+ });
+
 window.addEventListener("load", quiz.init);
